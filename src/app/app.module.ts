72e96e2927dfb760826b975efaf5641c06080387
Tim Gittos <tim@umuse.io>
2018-06-27 18:03:08 -0500

    Added basic API request to hero service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
@@ -17,7 +18,8 @@ import { AppRoutingModule } from './/app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+	  HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
  
 import { Observable, of } from 'rxjs';
  
@@ -11,11 +12,15 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
  
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient) { }
  
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    // return of(HEROES);
+    const url = 'https://tastedive.com/api/similar?q=Harry+Potter&info=1&limit=4&k=311621-NextBing-Q67NK7EY';
+    return this.http.get<Hero[]>(url);
   }
 }
